35b393f9877b370471b8fdf78d10168781b40a40
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2024-09-14 12:29:48 +0000

    Wire up to connect to google sheets using gspread library

--- run.py
+++ run.py
@@ -5,3 +5,20 @@ from tabulate import tabulate
 from simple_term_menu import TerminalMenu
 
 init(autoreset=True)
+
+def connect_to_google_sheets(my_finance_tracker):
+    """
+    Connect to google spreadsheet using gspread library
+    """
+    SCOPE = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive'
+        ]
+
+    CREDS = ServiceAccountCredentials.from_json_keyfile_name('creds.json', SCOPE)
+    CLIENT = gspread.authorize(CREDS)
+    SHEET = CLIENT.open(my_finance_tracker).sheet1
+    return SHEET
+
+SHEET = connect_to_google_sheets("my_finance_tracker")
