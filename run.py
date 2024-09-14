1e51e53467e7c8f3967d59649a1cc6fedde56de2
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2024-09-14 11:56:19 +0000

    Import added libraries and initialize Colorama

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from colorama import Fore, Style, init
+from tabulate import tabulate
+from simple_term_menu import TerminalMenu
+
+init(autoreset=True)
